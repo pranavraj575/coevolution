2247e6faa74f9a9825b7bbe5bf10da8b832f4dd2
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2024-07-05 01:22:51 -0400

    made team generation

--- src/team_builder.py
+++ src/team_builder.py
@@ -4,7 +4,64 @@ from torch.nn import Transformer
 from torch.nn import Embedding
 
 
-class TeamBuilder(nn.Module):
+class TeamBuilder:
+    def __init__(self, berteam=None, num_agents=None):
+        if berteam is None:
+            berteam = BERTeam(num_agents=num_agents)
+        self.berteam = berteam
+        self.optim = torch.optim.Adam(berteam.parameters())
+        self.MASK = berteam.MASK
+
+    def create_masked_teams(self, T, N=1):
+        """
+        Args:
+            T: team size
+            N: batch size
+        Returns:
+            an (N,T) tensor of all masked members
+        """
+        return torch.fill(torch.zeros((N, T), dtype=torch.long), self.berteam.MASK)
+
+    def mutate_add_member(self, initial_team, indices=None, input_embedding=None, input_mask=None, noise_model=None, ):
+        """
+        Args:
+            initial_team: initial torch array of team members, shape (N,T)
+            indices: indices to replace (if None, picks one masked index at random)
+                should be in pytorch format, (list of dim 0 indices, list of dim 1 indices)
+            input_embedding: input to give team builder, size (N,S,embedding_dim)
+                if None, gives dummy input and masks it
+            input_mask: size (N,S) boolean array of whether to mask each input embedding
+            noise_model: noise to add to probability distribution, if None, doesnt add noise
+        Returns:
+            team with updates
+        """
+        N, T = initial_team.shape
+        if indices is None:
+            potential = torch.where(initial_team == self.MASK)
+            if len(potential[0]) == 0:
+                print("warning: tried to add to full team")
+                return initial_team
+            idx = torch.randint(0, len(potential[0]), (1,))
+            indices = [[t[idx]] for t in potential]
+
+        if input_embedding is None:
+            input_embedding = torch.zeros((N, 1, self.berteam.embedding_dim))
+            input_mask = torch.ones((N, 1), dtype=torch.bool)
+        output = self.berteam.forward(input_embedding=input_embedding,
+                                      target_team=initial_team,
+                                      input_mask=input_mask,
+                                      output_probs=True,
+                                      )
+        dist = output[indices]  # (|indices|,num_agents) multinomial distribution for each index to update
+        if noise_model is not None:
+            # add noise if this is a thing
+            dist = noise_model(dist)
+        # torch.multinomial samples each
+        initial_team[indices] = torch.multinomial(dist, 1)
+        return initial_team
+
+
+class BERTeam(nn.Module):
     def __init__(self,
                  num_agents,
                  embedding_dim=512,
@@ -19,10 +76,10 @@ class TeamBuilder(nn.Module):
             dim_feedforward = self.embedding_dim*4
         self.num_agents = num_agents
         self.num_tokens = num_agents + 3
-        # adding [MSK], [CLS], and [CLS2] tokens ([CLS] is for target (team) sequences, [CLS2] is for inputs)
+        # adding [MASK], [CLS], and [CLS2] tokens ([CLS] is for target (team) sequences, [CLS2] is for inputs)
         self.CLS = num_agents
         self.CLS2 = num_agents + 1
-        self.MSK = num_agents + 1
+        self.MASK = num_agents + 2
         self.agent_embedding = Embedding(num_embeddings=self.num_tokens,
                                          embedding_dim=embedding_dim,
                                          )
@@ -32,7 +89,10 @@ class TeamBuilder(nn.Module):
                                      num_decoder_layers=num_decoder_layers,
                                      dim_feedforward=dim_feedforward,
                                      batch_first=True,  # takes in (N,S,E) input where N is batch and S is seq length
+                                     dropout=0.,
                                      )
+        self.output_layer = nn.Linear(embedding_dim, num_agents)
+        self.softmax = nn.Softmax(dim=-1)
 
     def add_cls_tokens(self, target_team):
         """
@@ -45,7 +105,7 @@ class TeamBuilder(nn.Module):
         (N, T) = target_team.shape
         return torch.cat((target_team, torch.ones((N, 1), dtype=target_team.dtype)*self.CLS), dim=1)
 
-    def forward(self, input_embedding, target_team, input_mask, target_mask):
+    def forward(self, input_embedding, target_team, input_mask, output_probs=True):
         """
         Args:
             input_embedding: (N, S, E) shape tensor of input
@@ -53,10 +113,13 @@ class TeamBuilder(nn.Module):
             target_team: (N, T) shape tensor of team members
                 EACH TEAM SHOULD END WITH A [CLS] token
             input_mask: (N, S) tensor of booleans on whether to mask each input
-            target_mask: (N, T) tensor of which team members are masked
+            output_probs: whether to output the probability of each team member
+                otherwise just outputs the final embedding
         Returns:
+            if output_probs, (N, T, num_agents) probability distribution for each position
+            otherwise, (N, T, embedding_dim) output of transformer model
         """
-        N, T = target_team.shape
+        # N, T = target_team.shape
         N, S, _ = input_embedding.shape
 
         if input_mask is None:
@@ -73,29 +136,30 @@ class TeamBuilder(nn.Module):
 
         target = self.agent_embedding(target_team)
 
-        # size (N, T, T), target i cannot see target j
-        tgt_mask = torch.zeros((N, T, T))  # should prevent everyone from seeing the masked team members
-        tgt_mask[torch.where(target_mask)] = -torch.inf
-
-        # size (N, S+1, S+1), source i cannot see source j
-        # src_mask = torch.zeros((N, S + 1, S + 1))  # should mask the maked elements of input embedding
-        # src_mask[torch.where(input_mask)]=-
-
-        # size (N, T, S+1), target i cannot see source j
-        memory_mask = torch.zeros((T, S + 1))  # shoult mask hidden input embeddings from the target
-        memory_mask = torch.fill(memory_mask, -torch.inf)
-
         output = self.transform.forward(src=source,
                                         tgt=target,
-                                        # tgt_mask=tgt_mask,
                                         src_key_padding_mask=input_mask,
-                                        # memory_key_padding_mask=input_mask,
+                                        memory_key_padding_mask=input_mask,
                                         )
+        if output_probs:
+            output = self.softmax(self.output_layer(output))
+
         return output
 
 
 if __name__ == '__main__':
+    N = 1
+    S = 10
+    T = 3
     torch.random.manual_seed(69)
+
+    test = TeamBuilder(num_agents=69)
+    init_team = test.create_masked_teams(T=T)
+    print('initial', init_team)
+    for _ in range(3):
+        test.mutate_add_member(init_team)
+        print(init_team)
+
     test = Transformer(d_model=16, batch_first=True)
     s = torch.rand((1, 5, 16))
     s[0, 0, :] = 100.
@@ -105,19 +169,23 @@ if __name__ == '__main__':
 
     # quit()
 
-    test = TeamBuilder(num_agents=69, embedding_dim=16, num_encoder_layers=10)
-    N = 3
-    S = 10
-    T = 5
+    test = BERTeam(num_agents=69, embedding_dim=16)
     E = test.embedding_dim
     team = torch.randint(0, test.num_agents, (N, T))
     team = test.add_cls_tokens(team)
 
-    target_mask = torch.zeros((N, T), dtype=torch.bool)
+    input_embedding = torch.ones((N, S, E))
+    input_mask = torch.ones((N, S), dtype=torch.bool)
+    input_mask[:, 0] = False
 
-    input_embedding = torch.ones((N, S, E))*999
-    print(test.forward(input_embedding=input_embedding,
-                       target_team=team,
-                       input_mask=torch.ones((N, S), dtype=torch.bool),
-                       target_mask=target_mask
-                       ))
+    a = test.forward(input_embedding=input_embedding,
+                     target_team=team,
+                     input_mask=input_mask
+                     )
+    input_embedding[:, 1] *= 10
+    b = test.forward(input_embedding=input_embedding,
+                     target_team=team,
+                     input_mask=input_mask,
+                     )
+
+    assert torch.all(a == b)
---
+++ tests/rps_basic.py
@@ -0,0 +1,5 @@
+
+
+
+if __name__=='__main__':
+    from src.team_builder import BERTeam
