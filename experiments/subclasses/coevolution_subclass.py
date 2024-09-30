acbf6e4d7112a6af4352d8e2d99902c364a79e06
pravna <god@example.com>
2024-09-29 20:12:12 -0400

    stuff

--- experiments/subclasses/coevolution_subclass.py
+++ experiments/subclasses/coevolution_subclass.py
@@ -335,7 +335,7 @@ class PZCC_MAPElites(ComparisionExperiment):
         loc_matrix = torch.linalg.norm(locations.view(-1, 1, loc_dim) -
                                        locations.view(1, -1, loc_dim),
                                        dim=-1,
-                                       ) + torch.diag(torch.inf*torch.ones(locations.shape[0]))
+                                       ) #+ torch.diag(torch.inf*torch.ones(locations.shape[0]))
         valid_locs = loc_matrix <= behavior_radius
         valid_locs = valid_locs/torch.sum(valid_locs, dim=0, keepdim=True)
         estimates = torch.matmul(elos.view(1, -1), valid_locs)
